57505c33ac432600f91fc2d34898c8028d557e0d
macakklett <macanvitalijj@gmail.com>
2023-11-08 13:20:42 +0200

    side effect, pure fn without get request

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,22 +5,16 @@ import axios from 'axios';
 
 const Users = (props) => {
     // debugger;
-    if(props.users.length === 0){
-
+    const getUsers = () => {
+        if(props.users.length === 0){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            // debugger;
             props.setUsers(response.data.items);
         });
+    }}
+    
 
-        // props.setUsers([
-        //         { id: 1, followed: true, fullName: 'Vasiliy', status: "Hello, how are you", location: {country: 'Ukraine', city: 'Nk'} },
-        //         { id: 2, followed: false, fullName: 'Ignat', status: "Hello, i`m speaker of fly powers", location: {country: 'Ukraine', city: 'Zp'} },
-        //         { id: 3, followed: true, fullName: 'Sveta', status: "Hello, i studied at the technicum", location: {country: 'Ukraine', city: 'Dp'} },
-        //         { id: 4, followed: false, fullName: 'Nadya', status: "Hello, i`m very smart", location: {country: 'Ukraine', city: 'Kv'} }
-        //     ]
-        // )
-    }
     return <div>
+    <button onClick={getUsers}>Show users</button>
         {props.users.map((user) => {
             return <div className={styles.user} key={user.id}>
                         <div className={styles.userCard}>
@@ -47,3 +41,13 @@ const Users = (props) => {
 }
 
 export default Users;
+
+
+
+        // props.setUsers([
+        //         { id: 1, followed: true, fullName: 'Vasiliy', status: "Hello, how are you", location: {country: 'Ukraine', city: 'Nk'} },
+        //         { id: 2, followed: false, fullName: 'Ignat', status: "Hello, i`m speaker of fly powers", location: {country: 'Ukraine', city: 'Zp'} },
+        //         { id: 3, followed: true, fullName: 'Sveta', status: "Hello, i studied at the technicum", location: {country: 'Ukraine', city: 'Dp'} },
+        //         { id: 4, followed: false, fullName: 'Nadya', status: "Hello, i`m very smart", location: {country: 'Ukraine', city: 'Kv'} }
+        //     ]
+        // )
